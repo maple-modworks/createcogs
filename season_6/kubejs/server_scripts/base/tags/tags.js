bd358cd4ec4e74dd48222f2393a319bd7ed8458b
tizu <60812901+tizu69@users.noreply.github.com>
2024-11-24 22:53:53 +0100

    remove dnd the first
    Co-authored-by: Herr Katze <34895975+scmcgowen@users.noreply.github.com>

--- season_6/kubejs/server_scripts/base/tags/tags.js
+++ season_6/kubejs/server_scripts/base/tags/tags.js
@@ -1,5 +1,4 @@
 ServerEvents.tags("block", (event) => {
 	event.add("minecraft:beacon_base_blocks", "kubejs:refined_radiance_block");
-	event.add("create_dd:asphalt_blocks", "createdieselgenerators:asphalt_block");
 	event.add("kubejs:ropes", ["farmersdelight:rope", "supplementaries:rope"]);
 });
