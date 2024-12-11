a777ba2e3e976a7f72c9d36c8391c70b4655126e
scmcgowen <scmcgowen@gmail.com>
2024-12-10 23:30:49 -0800

    Fix broken IDs for ae2 transformation recipes

--- season_6/kubejs/server_scripts/base/recipes/ae2/transform.js
+++ season_6/kubejs/server_scripts/base/recipes/ae2/transform.js
@@ -2,14 +2,10 @@ ServerEvents.recipes((event) => {
 	const r = (ingredients, result, count) =>
 		event.custom({
 			type: "ae2:transform",
-			circumstance: {
-				type: "fluid",
-				tag: "minecraft:water",
-			},
 			ingredients: ingredients.map((i) => ({ item: i})),
 			result: {item: result, count: count || 1},
 		});
 
-	r(["nether_quartz", "minecraft:glowstone_dust", "create:refined_radiance"], "ae2:fluix_crystal", 3);
-	r(["nether_quartz", "redstone_dust", "ae2:charged_certus_quartz_crystal"], "ae2:fluix_crystal");
+	r(["minecraft:quartz", "minecraft:glowstone_dust", "create:refined_radiance"], "ae2:fluix_crystal", 3);
+	r(["minecraft:quartz", "minecraft:redstone", "ae2:charged_certus_quartz_crystal"], "ae2:fluix_crystal");
 });
